8bc748a581764b20af1d7b06c4450036f5afcf61
Roman Kurakin <kurakinroman1606@gmail.com>
2024-01-01 14:14:35 +0300

    homework l2 t2

---
+++ Lesson_1/Lesson_2/Homework/h2_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+        Console.WriteLine("Введите координаты точки (X и Y):");
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine("Точка находится в первой координатной четверти");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("Точка находится во второй координатной четверти");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("Точка находится в третьей координатной четверти");
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine("Точка находится в четвертой координатной четверти");
+        }
+        else
+        {
+            Console.WriteLine("Некорректные координаты точки. Пожалуйста, введите X ≠ 0 и Y ≠ 0.");
+        }
