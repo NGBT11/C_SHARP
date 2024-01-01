165b4aef7c7f0401a4cf281315f853980c300762
Roman Kurakin <kurakinroman1606@gmail.com>
2024-01-01 13:01:10 +0300

    Lesson 1 task 1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго.
+
+string a1 =  Console.ReadLine()!;
+string a2 =  Console.ReadLine()!;
+int b1 =   Convert.ToInt32(a1);
+int b2 =   Convert.ToInt32(a2);
+if (b1 * b1 == b2)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
