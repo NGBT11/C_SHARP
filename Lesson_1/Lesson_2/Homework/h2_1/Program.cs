70a9e93b579174860b2a38469a70da4b0c88c0e6
Roman Kurakin <kurakinroman1606@gmail.com>
2024-01-01 14:02:46 +0300

    homework l2 t1

---
+++ Lesson_1/Lesson_2/Homework/h2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Напишите программу, которая принимает
+//на вход число и проверяет, кратно ли оно
+//одновременно 7 и 23
+
+Console.WriteLine("Введите число:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 7 == 0 && number % 23 == 0)
+        {
+            Console.WriteLine("кратно");
+        }
+        else
+        {
+            Console.WriteLine("не кратно");
+        }
