3570a627758d5530e131f9f5e981ed37958c112d
Roman Kurakin <kurakinroman1606@gmail.com>
2024-01-01 13:57:41 +0300

    l2 t4

---
+++ Lesson_1/Lesson_2/2_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+ Console.WriteLine("Введите число");
+ long num = Convert.ToInt32(Console.ReadLine());
+
+ if (num < 100)
+ {
+ Console.WriteLine("третьей цыфры нет");
+ }
+
+ if (num >= 1000)
+ {
+ long num1 = (num / 100) % 10;
+ Console.WriteLine(num1);
+ }
+
+ else if (num >= 100 && num < 1000)
+ {
+ long num2 = num % 10;
+ Console.WriteLine(num2);
+  }
