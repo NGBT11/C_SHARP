3c6bce31d5d952a804b3067b35e4cb41270b048a
Roman Kurakin <kurakinroman1606@gmail.com>
2024-01-01 13:45:14 +0300

    l2 t2

---
+++ Lesson_1/Lesson_2/2_2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = int.Parse(Console.ReadLine()!);
+int n3 = n % 10;
+int n2 = n / 10 % 10;
+Console.WriteLine(Math.Pow(n2, n3));
