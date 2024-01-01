ec6d626bea91190f9ccbbd6388d9b9c6fc27eebf
Roman Kurakin <kurakinroman1606@gmail.com>
2024-01-01 14:16:57 +0300

    homework l2 t3

---
+++ Lesson_1/Lesson_2/Homework/h2_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+        Console.WriteLine("Введите целое число из отрезка [10, 99]:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number >= 10 && number <= 99)
+        {
+            int maxDigit = 0;
+            int currentDigit;
+
+            while (number > 0)
+            {
+                currentDigit = number % 10;
+                if (currentDigit > maxDigit)
+                {
+                    maxDigit = currentDigit;
+                }
+                number /= 10;
+            }
+
+            Console.WriteLine("Наибольшая цифра числа: " + maxDigit);
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число из отрезка [10, 99].");
+        }
