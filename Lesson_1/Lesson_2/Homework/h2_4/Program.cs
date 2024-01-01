97645d7715185351f6b441366240cf14b2a0d27a
Roman Kurakin <kurakinroman1606@gmail.com>
2024-01-01 14:22:03 +0300

    homework l2 t4

---
+++ Lesson_1/Lesson_2/Homework/h2_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+        Console.WriteLine("Введите натуральное число:");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n > 0)
+        {
+            string digitsString = "";
+
+            while (n > 0)
+            {
+                int digit = n % 10;
+                digitsString = digit + ", " + digitsString;
+                n /= 10;
+            }
+
+            digitsString = digitsString.TrimEnd(',', ' ');
+            Console.WriteLine("Цифры: " + digitsString);
+        }
+        else
+        {
+            Console.WriteLine("ВВЕДИТЕ НАТУРАЛЬНОЕ ЧИСЛО!!.");
+        };
