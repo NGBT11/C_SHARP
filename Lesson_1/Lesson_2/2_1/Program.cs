7984fd1631d510eaade5190130ffe63857b44c91
Roman Kurakin <kurakinroman1606@gmail.com>
2024-01-01 13:36:38 +0300

    l2 t1

---
+++ Lesson_1/Lesson_2/2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// 1. Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого числа.
+
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(a / 100 * 10 + a % 10);
