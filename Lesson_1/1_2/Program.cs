afc02f2bddccb64e01573340e97729e72664463d
Roman Kurakin <kurakinroman1606@gmail.com>
2024-01-01 13:13:15 +0300

    lesson 1 task 2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// 2. Напишите программу, которая на вход принимает одно число
+// (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+
+Console.Write("Enter A Number");
+int a = int.Parse(Console.ReadLine()!);
+
+int count = a * 2 + 1;
+int i = 1;
+a = a * -1;
+Console.Write(a);
+Console.Write(' ');
+
+while (i < count)
+{
+a = a + 1;
+Console.Write(a);
+Console.Write(' ');
+i = i + 1;
+}
